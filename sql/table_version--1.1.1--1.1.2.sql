41f93ad70fa1b942ce5068f06240ec6df4d245f0
Jeremy Palmer <jpalmer@linz.govt.nz>
2016-05-02 14:44:46 +1200

    Added version upgrade script

---
+++ sql/table_version--1.1.1--1.1.2.sql
@@ -0,0 +1,15 @@
+--------------------------------------------------------------------------------
+
+-- postgresql-table_version - PostgreSQL database patch change management extension
+--
+-- Copyright 2016 Crown copyright (c)
+-- Land Information New Zealand and the New Zealand Government.
+-- All rights reserved
+--
+-- This software is released under the terms of the new BSD license. See the 
+-- LICENSE file for more information.
+--
+--------------------------------------------------------------------------------
+
+-- No SQL changes between 1.1.1 and 1.1.2
+
