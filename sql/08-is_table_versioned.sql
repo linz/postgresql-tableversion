0fd76a6fd9616c30a88947bba5e7496b42509e10
Sandro Santilli <strk@kbt.io>
2017-09-28 09:09:29 +0200

    Have ver_is_table_versioned return false when triggers are lost
    This happens on drop and re-create of a table.
    See #57

--- sql/08-is_table_versioned.sql
+++ sql/08-is_table_versioned.sql
@@ -1,28 +1,72 @@
 
+CREATE OR REPLACE FUNCTION _ver_is_table_versioned(
+    p_schema NAME,
+    p_table  NAME
+)
+RETURNS BOOLEAN AS
+$$
+    SELECT EXISTS (
+      SELECT
+          versioned
+      FROM
+          @extschema@.versioned_tables
+      WHERE
+          schema_name = p_schema AND
+          table_name = p_table
+    )
+$$ LANGUAGE sql STABLE;
+
+-- {
 CREATE OR REPLACE FUNCTION ver_is_table_versioned(
     p_schema NAME,
     p_table  NAME
 )
-RETURNS BOOLEAN AS 
+RETURNS BOOLEAN AS
 $$
 DECLARE
     v_is_versioned BOOLEAN;
+    v_has_trigger BOOLEAN;
 BEGIN
     SELECT
         versioned
     INTO
         v_is_versioned
-    FROM 
-        @extschema@.versioned_tables 
+    FROM
+        @extschema@.versioned_tables
     WHERE
         schema_name = p_schema AND
         table_name = p_table;
 
     IF v_is_versioned IS NULL THEN
-        v_is_versioned := FALSE;
+        RETURN FALSE;
+    END IF;
+
+    -- Check that table exists and triggers are in place;
+    -- warn otherwise, see:
+    -- https://github.com/linz/postgresql-tableversion/issues/57
+    BEGIN
+      v_is_versioned := EXISTS (
+        SELECT *
+      FROM pg_trigger t
+      WHERE t.tgrelid = (quote_ident(p_schema) || '.' || quote_ident(p_table) )::regclass
+        AND t.tgname = p_schema || '_' || p_table || '_revision_trg'
+      );
+    EXCEPTION
+      WHEN UNDEFINED_TABLE THEN
+        RAISE WARNING 'Table %.% does not exist', p_schema, p_table;
+        RETURN FALSE;
+      WHEN OTHERS THEN
+        RAISE EXCEPTION 'Got % (%)', SQLERRM, SQLSTATE;
+    END;
+
+    IF NOT v_is_versioned THEN
+        RAISE WARNING 'Table %.% is known as versioned but lacks versioning triggers',
+          p_schema, p_table
+        USING HINT = 'use ver_create_version_trigger to recover';
     END IF;
 
     RETURN v_is_versioned;
 END;
-$$ LANGUAGE plpgsql;
+$$ LANGUAGE plpgsql STABLE;
+-- }
 
