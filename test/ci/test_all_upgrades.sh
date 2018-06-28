f5050dffcdb61059126595aa4167844199ba090c
Sandro Santilli <strk@kbt.io>
2018-06-28 16:16:02 +0200

    Make sure to always use latest loader

--- test/ci/test_all_upgrades.sh
+++ test/ci/test_all_upgrades.sh
@@ -9,6 +9,10 @@ cd `dirname $0`/../../
 VER="1.1.0 1.1.1 1.1.2 1.1.3 1.2.0 1.3.0 1.3.1 1.4.0 1.4.1 1.4.2 1.4.3";
 
 TMP_INSTALL_DIR_PREFIX=/tmp/table_version
+mkdir -p ${TMP_INSTALL_DIR_PREFIX}
+
+# Save current table_version
+cp -a `which table_version-loader` ${TMP_INSTALL_DIR_PREFIX}
 
 # Install all older versions
 git fetch --unshallow --tags # to get all commits/tags
@@ -24,6 +28,10 @@ for v in $VER; do
 done;
 cd ..
 rm -rf older-versions
+
+# Restore current table_version after installing/overriding new one
+# (effectively moving to wherever will be found first)
+cp -a ${TMP_ISTALL_DIR_PREFIX} `which table_version-loader`
 
 # Test upgrade from all older versions
 for v in $VER; do
