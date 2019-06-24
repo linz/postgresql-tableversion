e3d2eec62dd16dba946b62cec0a5f6da3deae4d6
Sandro Santilli <strk@kbt.io>
2019-06-24 11:21:32 +0200

    [upgrade-tester] patch Makefile of old versions to make it work

--- test/ci/test_all_upgrades.sh
+++ test/ci/test_all_upgrades.sh
@@ -26,7 +26,11 @@ for v in $VER; do
   echo "-------------------------------------"
   echo "Installing version $v"
   echo "-------------------------------------"
+  git checkout . # revert local patches
   git checkout $v && git clean -dxf || exit 1
+  # Workaround for Makefile bug which was fixed by
+  # 2dee5082e0e89e4cf2430b566e8013ac1afd92be...
+  sed -ie '/echo .*load this file/{s/echo /printf /;s|\\|\\\\|g}' Makefile
   # Since 1.4.0 we have a loader
   if test `echo $v | tr -d .` -ge 140; then
     TPL_INSTALL_DIR=`make install | grep tpl | tail -1 | sed "s/.* //;s/'$//;s/^'//"`
