b8ae04d87866d3480d2aa43b6da81399be11e523
Victor Engmark <vengmark@linz.govt.nz>
2022-01-26 10:39:29 +1300

    refactor: Use consistent 4-space indentation

--- test/ci/test_all_upgrades.bash
+++ test/ci/test_all_upgrades.bash
@@ -35,24 +35,24 @@ git clone . older-versions
 cd older-versions || exit 1
 for version in "${versions[@]}"
 do
-  echo "-------------------------------------"
-  echo "Installing version ${version}"
-  echo "-------------------------------------"
-  git checkout . # revert local patches
-  git checkout "$version" && git clean -dxf || exit 1
-  # Workaround for Makefile bug which was fixed by
-  # 2dee5082e0e89e4cf2430b566e8013ac1afd92be...
-  sed -ie '/echo .*load this file/{s/echo /printf /;s|\\|\\\\|g}' Makefile
-  # Since 1.4.0 we have a loader
-  if test "$(echo "$version" | tr -d .)" -ge 140
-  then
-    tpl_install_dir="$(make install | grep tpl | tail -1 | sed "s/.* //;s/'$//;s/^'//")"
-    test -n "$tpl_install_dir" || exit 1
-    mkdir -p "${tmp_install_dir_prefix}/${version}/share" || exit 1
-    cp -f "${tpl_install_dir}/"*.tpl "${tmp_install_dir_prefix}/${version}/share" || exit 1
-  else
-    make install || exit 1
-  fi
+    echo "-------------------------------------"
+    echo "Installing version ${version}"
+    echo "-------------------------------------"
+    git checkout . # revert local patches
+    git checkout "$version" && git clean -dxf || exit 1
+    # Workaround for Makefile bug which was fixed by
+    # 2dee5082e0e89e4cf2430b566e8013ac1afd92be...
+    sed -ie '/echo .*load this file/{s/echo /printf /;s|\\|\\\\|g}' Makefile
+    # Since 1.4.0 we have a loader
+    if test "$(echo "$version" | tr -d .)" -ge 140
+    then
+        tpl_install_dir="$(make install | grep tpl | tail -1 | sed "s/.* //;s/'$//;s/^'//")"
+        test -n "$tpl_install_dir" || exit 1
+        mkdir -p "${tmp_install_dir_prefix}/${version}/share" || exit 1
+        cp -f "${tpl_install_dir}/"*.tpl "${tmp_install_dir_prefix}/${version}/share" || exit 1
+    else
+        make install || exit 1
+    fi
 done
 cd ..
 rm -rf older-versions
@@ -64,20 +64,20 @@ cp -a "${tmp_install_dir_prefix}/table_version-loader" "$(which table_version-lo
 # Test upgrade from all older versions
 for version in "${versions[@]}"
 do
-  echo "-------------------------------------"
-  echo "Checking upgrade from version ${version}"
-  echo "-------------------------------------"
-  make installcheck-upgrade PREPAREDB_UPGRADE_FROM="$version" || exit 1
-  # Since 1.4.0 we have a loader
-  if test "$(echo "$version" | tr -d .)" -ge 140
-  then
-    make installcheck-loader-upgrade \
-      PREPAREDB_UPGRADE_FROM="$version" \
-      PREPAREDB_UPGRADE_FROM_EXT_DIR="${tmp_install_dir_prefix}/${version}/share" \
-      || exit 1
-    make installcheck-loader-upgrade-noext \
-      PREPAREDB_UPGRADE_FROM="$version" \
-      PREPAREDB_UPGRADE_FROM_EXT_DIR="${tmp_install_dir_prefix}/${version}/share" \
-      || exit 1
-  fi
+    echo "-------------------------------------"
+    echo "Checking upgrade from version ${version}"
+    echo "-------------------------------------"
+    make installcheck-upgrade PREPAREDB_UPGRADE_FROM="$version" || exit 1
+    # Since 1.4.0 we have a loader
+    if test "$(echo "$version" | tr -d .)" -ge 140
+    then
+        make installcheck-loader-upgrade \
+            PREPAREDB_UPGRADE_FROM="$version" \
+            PREPAREDB_UPGRADE_FROM_EXT_DIR="${tmp_install_dir_prefix}/${version}/share" ||
+            exit 1
+        make installcheck-loader-upgrade-noext \
+            PREPAREDB_UPGRADE_FROM="$version" \
+            PREPAREDB_UPGRADE_FROM_EXT_DIR="${tmp_install_dir_prefix}/${version}/share" ||
+            exit 1
+    fi
 done
