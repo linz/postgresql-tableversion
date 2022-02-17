ad2220c85230e86808cfb9890b92ecdf5708cbed
Victor Engmark <vengmark@linz.govt.nz>
2022-02-17 14:58:42 +1300

    refactor: Pull out script to create extension SQL

---
+++ create-extension-sql.bash
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+set -o errexit -o noclobber -o nounset -o pipefail
+shopt -s failglob inherit_errexit
+
+printf '\echo Use "CREATE EXTENSION %s" to load this file. \quit\n' "$1"
+shift
+cat "$@"
+echo "GRANT USAGE ON SCHEMA table_version TO public;"
