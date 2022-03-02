6c3ecf94f1f9e38e163ae5e6585f49ef290882b8
Victor Engmark <vengmark@linz.govt.nz>
2022-03-02 13:12:58 +1300

    refactor: Move safety pragmas to executables
    Puts them right at the start of execution, and makes sure they don't rely
    on sub-scripts.

--- test/ci/upgrade-using-loader.bash
+++ test/ci/upgrade-using-loader.bash
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+set -o errexit -o noclobber -o nounset -o pipefail -o xtrace
+shopt -s failglob inherit_errexit
+
 script_dir="$(dirname "${BASH_SOURCE[0]}")"
 
 # shellcheck source=test/ci/install-latest.bash
