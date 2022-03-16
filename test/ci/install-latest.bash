802518c0a691bad4466413e8eec7d1a02cc51814
Victor Engmark <vengmark@linz.govt.nz>
2022-03-16 13:29:34 +1300

    test: Verify package upgrade
    Using locally built packages.
    
    Archive the packages if the test fails, for inspection.

--- test/ci/install-latest.bash
+++ test/ci/install-latest.bash
@@ -8,3 +8,5 @@ apt-get --assume-yes install "$package_name"
 installed_package_version="$(dpkg-query --showformat='${Version}' --show "$package_name" | cut --delimiter=- --fields=1)"
 # shellcheck disable=SC2034
 readonly installed_package_version
+
+su --command='psql --command="CREATE EXTENSION table_version"' postgres
