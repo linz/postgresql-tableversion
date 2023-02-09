67192eab8f9ef5cf0379129579732f9d964f1085
Victor Engmark <vengmark@linz.govt.nz>
2023-02-09 15:00:26 +1300

    build: Bump nixpkgs

--- shell.nix
+++ shell.nix
@@ -1,13 +1,14 @@
-{ pkgs ? import
-    (
-      fetchTarball {
-        name = "21.11";
-        url = "https://github.com/NixOS/nixpkgs/archive/a7ecde854aee5c4c7cd6177f54a99d2c1ff28a31.tar.gz";
-        sha256 = "162dywda2dvfj1248afxc45kcrg83appjd0nmdb541hl7rnncf02";
-      })
-    { }
-}:
 let
+  pkgs =
+    import
+      (
+        fetchTarball (
+          builtins.fromJSON (
+            builtins.readFile ./nixpkgs.json
+          )
+        )
+      )
+      { };
   postgresql = pkgs.postgresql;
   pg_prove = pkgs.stdenv.mkDerivation {
     name = "pg_prove";
